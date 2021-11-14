0998255a239ed7c6295f27531ddd19b23fc078d2
Levi Middleton <levi@theawesomefiles.com>
2021-11-14 11:00:18 -0500

    Added recipe index script

---
+++ recipes/name.ps1
@@ -0,0 +1,8 @@
+$textInfo = (Get-Culture).TextInfo
+$Output = Get-ChildItem -Exclude name.ps1, index.md | Sort-Object | ForEach-Object {
+    $BaseName = $_.BaseName
+	$TitleName = $textInfo.ToTitleCase($_.BaseName)
+
+    "[$TitleName](https://levi-middleton.github.io/writings/recipes/$BaseName)"
+}
+$Output | Out-File index.md
