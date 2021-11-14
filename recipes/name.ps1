2eb0874ca4704934e5990959e8b1994177254bc5
Levi Middleton <levi@theawesomefiles.com>
2021-11-14 11:09:05 -0500

    Added changes to index script

--- recipes/name.ps1
+++ recipes/name.ps1
@@ -1,8 +1,8 @@
 $textInfo = (Get-Culture).TextInfo
 $Output = Get-ChildItem -Exclude name.ps1, index.md | Sort-Object | ForEach-Object {
-    $BaseName = $_.BaseName
+    $BaseName = $_.BaseName -replace '(\s)','%20'
 	$TitleName = $textInfo.ToTitleCase($_.BaseName)
 
-    "[$TitleName](https://levi-middleton.github.io/writings/recipes/$BaseName)"
+    "- [$TitleName](https://levi-middleton.github.io/writings/recipes/$BaseName)"
 }
 $Output | Out-File index.md
